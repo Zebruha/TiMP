744fe1775e54f173ff8b69e43dcdae2d46493af3
Irina <zolotukhina.i.s@gmail.com>
2022-03-07 20:28:33 +0700

    В LR 3.1 добавлена проверка на корректность ввода данных

--- LR 3/LR 3.1.py
+++ LR 3/LR 3.1.py
@@ -1,7 +1,16 @@
 fio = input("Ваши фамилия, имя, отчество? ")
-mfio = input("Фамилия, имя, отчество Вашей мамы? " )
-ofio = input("Фамилия, имя, отчество Вашего отца? " )
-age = int(input("Сколько Вам лет? "))
+mfio = input("Фамилия, имя, отчество Вашей мамы? ")
+ofio = input("Фамилия, имя, отчество Вашего отца? ")
+def main(): #Проверка на корректность ввода данных
+   while True:
+        try:
+            age = int(input("Ваш возраст? "))
+            if age <= 0:
+                raise ValueError
+            return age
+        except ValueError:
+            print("Вы должны ввести неторицательное число")
+age = main()
 place = input("Где вы Живете? ")
 print("\n" + "Ваши фамилия, имя, отчество: ", fio)
 print("Вашу маму зовут:", mfio)
